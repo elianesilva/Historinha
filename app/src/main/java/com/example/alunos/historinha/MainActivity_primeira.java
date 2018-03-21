d6620699d667c1d989f0fc8de077011bcba9629c
Eliane <elyanesilva96@gmail.com>
2018-03-21 17:10:06 -0300

    Segunda Atualização

--- app/src/main/java/com/example/alunos/historinha/MainActivity_primeira.java
+++ app/src/main/java/com/example/alunos/historinha/MainActivity_primeira.java
@@ -1,13 +1,43 @@
 package com.example.alunos.historinha;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity_primeira extends AppCompatActivity {
+
+    EditText texto1;
+    EditText texto2;
+    Button btn1;
+    String  editTxtVal1;
+    String editTxtVal2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_primeira);
+
+        setRequestedOrientation((ActivityInfo.SCREEN_ORIENTATION_PORTRAIT));
+
+
+        texto1 = (EditText) findViewById(R.id.et_nome01);
+        texto2 = (EditText) findViewById(R.id.et_nome02);
+        btn1 = (Button) findViewById(R.id.bt_enviar);
+
     }
+    public void btnClickAct1(View v){
+
+        Intent i = new Intent(this, MainActivity_segunda.class);
+        editTxtVal1 = texto1.getText().toString();
+        i.putExtra("value",editTxtVal1);
+        editTxtVal2 = texto2.getText().toString();
+        i.putExtra("value", editTxtVal2);
+        startActivity(i);
+        finish();
+    }
+
 }
--- app/src/main/java/com/example/alunos/historinha/MainActivity_segunda.java
+++ app/src/main/java/com/example/alunos/historinha/MainActivity_segunda.java
@@ -1,13 +1,45 @@
 package com.example.alunos.historinha;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity_segunda extends AppCompatActivity {
+
+
+    TextView mostrarTexto1;
+    TextView mostrarTexto2;
+    Button btn2;
+    String valFromAct1;
+    String valFromAct2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_segunda);
+        setContentView(R.layout.activity_segunda);
+
+        setRequestedOrientation((ActivityInfo.SCREEN_ORIENTATION_PORTRAIT));
+
+
+        btn2 = (Button) findViewById(R.id.button1);
+
+        mostrarTexto1 = (TextView) findViewById(R.id.text3);
+        valFromAct1 = getIntent().getExtras().getString("value");
+        mostrarTexto1.setText(valFromAct1);
+
+        mostrarTexto2 = (TextView) findViewById(R.id.text4);
+        valFromAct2 = getIntent().getExtras().getString("value");
+        mostrarTexto2.setText(valFromAct2);
+
     }
+    public void btnClick2(View v){
+        Intent i = new Intent(this, TerceiraActivity.class);
+        startActivity(i);
+        finish();
+    }
+
 }
