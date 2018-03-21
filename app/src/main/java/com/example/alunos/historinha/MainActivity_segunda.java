a2d444c8e051a047357af9aee4dc465acee50aa9
Eliane <elyanesilva96@gmail.com>
2018-03-21 14:18:04 -0300

    Primeiro Envio

---
+++ app/src/main/java/com/example/alunos/historinha/MainActivity_segunda.java
@@ -0,0 +1,13 @@
+package com.example.alunos.historinha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity_segunda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_segunda);
+    }
+}
